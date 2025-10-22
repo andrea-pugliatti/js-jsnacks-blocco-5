0b31b7bb95960adc1fb20163283595ff8a2ffde9
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-22 14:03:29 +0200

    Add solution with find in snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,31 @@
 const students = [
-  {
-    id: 1,
-    name: 'Marco Lanci',
-    age: 32,
-    class: '3C'
-  },
-  {
-    id: 2,
-    name: 'Mario Banfi',
-    age: 34,
-    class: '4A'
-  },
-  {
-    id: 3,
-    name: 'Luigi Banzi',
-    age: 33,
-    class: '5B'
-  },
+	{
+		id: 1,
+		name: "Marco Lanci",
+		age: 32,
+		class: "3C",
+	},
+	{
+		id: 2,
+		name: "Mario Banfi",
+		age: 34,
+		class: "4A",
+	},
+	{
+		id: 3,
+		name: "Luigi Banzi",
+		age: 33,
+		class: "5B",
+	},
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+// Soluzione find
+let student = students.find((current) => current.name === "Marco Lanci");
+let studentClass = student.class;
+// Avrei potuto fare
+// let studentClass = students.find((current) => current.name === "Marco Lanci").class;
+
+console.log(student, studentClass);
