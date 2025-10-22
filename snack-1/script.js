cc8345ec2c021be1ec7744b0a2cf9ee22b4d3963
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-22 13:23:59 +0200

    Add solution with for loop in snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -7,3 +7,8 @@ const names = ["Edoardo", "Simone", "Francesco"];
 names.forEach((current) => {
 	console.log(current);
 });
+
+// Soluzione for
+for (let i = 0; i < names.length; i++) {
+	console.log(names[i]);
+}
